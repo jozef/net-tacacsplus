d6a4d48611604bd1f0972f5f1d1d2492edad3502
jk <jk@b0c1b02c-7ba7-4df7-b273-855bf36df2ab>
2007-09-08 19:12:29 +0000

    added experimental POE
    git-svn-id: https://cle.sk/repos/pub/cpan/Net-TacacsPlus/trunk@114 b0c1b02c-7ba7-4df7-b273-855bf36df2ab

---
+++ lib/POE/Filter/TacacsPlus.pm
@@ -0,0 +1,41 @@
+package POE::Filter::TacacsPlus;
+
+sub new {
+	my $class = shift;
+	
+	my $self = {};
+	
+	bless $self, $class;
+	
+	return $self;
+}
+
+=head1 METHODS
+
+=over 4
+
+=item get()
+
+Transforms raw packets to the Net::TacacsPlus::Packet object.
+
+=cut
+
+sub get {
+	my $self = shift;
+}
+
+=item pub()
+
+Trnasforms Net::TacacsPlus::Packet to the binary packet form.
+
+=cut
+
+sub put {
+	my $self = shift;
+}
+
+=back
+
+=cut
+
+1;
